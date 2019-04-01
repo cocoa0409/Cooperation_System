badfae6432f542b00c254a6a66e5cb5ed589bdc6
zjwang <zjwang@buaa.edu.cn>
2019-04-01 10:00:02 +0800

    initial status

---
+++ cooperation_system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cooperation_system
+//
+//  Created by 兆吉 王 on 2019/4/1.
+//  Copyright © 2019 兆吉 王. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
